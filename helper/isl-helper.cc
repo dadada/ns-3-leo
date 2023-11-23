4dbec3912c52af1a71bbbc1d72abafc25b00dce4
Tim Schubert <dadada@dadada.li>
2023-11-23 09:38:39 +0100

    Fix licensing info

--- helper/isl-helper.cc
+++ helper/isl-helper.cc
@@ -1,7 +1,5 @@
 /* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
 /*
- * Copyright (c) 2008 INRIA
- *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 as
  * published by the Free Software Foundation;
@@ -15,7 +13,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
- * Author: Mathieu Lacage <mathieu.lacage@sophia.inria.fr>
+ * Author: Tim Schubert <ns-3-leo@timschubert.net>
  */
 
 #include "ns3/abort.h"
--- model/mock-net-device.h
+++ model/mock-net-device.h
@@ -1,7 +1,5 @@
 /* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
 /*
- * Copyright (c) 2007, 2008 University of Washington
- *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 as
  * published by the Free Software Foundation;
@@ -14,6 +12,8 @@
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * Author: Tim Schubert <ns-3-leo@timschubert.net>
  */
 
 #ifndef MOCK_NET_DEVICE_H
